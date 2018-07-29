8551921c7e6d682cbe21e9bd09bcd6d0604d2f72
Mario Tacke <tacke.mario@gmail.com>
2018-07-29 13:48:41 -0700

    feat: add socket.io-redis adapter

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const http = require('http');
 const io = require('socket.io')();
 const socketAuth = require('socketio-auth');
+const adapter = require('socket.io-redis');
 
 const PORT = process.env.PORT || 9000;
 const server = http.createServer();
 
+const redisAdapter = adapter({
+  host: process.env.REDIS_HOST || 'localhost',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASS || 'password',
+});
+
 io.attach(server);
+io.adapter(redisAdapter);
 
 // dummy user verification
 async function verifyUser (token) {
